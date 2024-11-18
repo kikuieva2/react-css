a9a20214eff0fab15a6a218b982fa1e7e9db8b16
kikuieva2 <140496966+kikuieva2@users.noreply.github.com>
2024-11-18 17:47:57 +0200

    Update CardStyleTrafic.jsx

--- latest/src/components/CardStyleTrafic.jsx
+++ latest/src/components/CardStyleTrafic.jsx
@@ -1,13 +1,43 @@
-import TarifCards from "./TarifCards.module.css";
+i
+import React from "react";
+import TarifCards from "./TarifCards";
 
-function CardStyleTrafic(){
-    return(
-        <>
-        <TarifCards title="Безлимитный 300" price="руб 300 / мес" megabaits="до 10 Мбит/сек" description="Объем включенного трафика не ограничен" colorClass="blueCard" />
-        <TarifCards title="Безлимитный 300" price="руб 450 / мес" megabaits="до 50 Мбит/сек" description="Объем включенного трафика не ограничен" colorClass = "greenCard"/>
-        <TarifCards title="Безлимитный 300" price="руб 550 / мес" megabaits="до 100 Мбит/сек" description="Объем включенного трафика не ограничен" colorClass = "redCard"/>
-        <TarifCards title="Безлимитный 300" price="руб 1000 / мес" megabaits="до 200 Мбит/сек" description="Объем включенного трафика не ограничен" colorClass="blackCard"/>
-        </>
-    )
+const tarifData = [
+    {
+        title:"Безлимитный 300",
+        price: "руб 300 / мес", 
+        megabaits: "до 10 Мбит/сек",
+        description:"Объем включенного трафика не ограничен", 
+        colorClass:"blueCard"},
+    {
+        title: "Безлимитный 450",
+        price:"руб 450 / мес",
+        megabaits: "до 50 Мбит/сек",
+        description: "Объем включенного трафика не ограничен",
+        colorClass: "greenCard"
+    },
+        {
+            title: "Безлимитный 550",
+            price: "руб 550 / мес",
+            megabaits:"до 100 Мбит/сек",
+            description: "Объем включенного трафика не ограничен",
+            isHighlighted: true,
+            colorClass:"redCard",
+        },
+            { title: "Безлимитный 1000",
+                price: "руб 1000 / мес",
+                megabaits: "до 200 Мбит/сек",
+                description:"Объем включенного трафика не ограничен",
+                colorClass: "blackCard",
+                isHighlighted:false,
+            }
+]
+export default function TarifCardsList() {
+return (
+    <React.Fragment>
+      {tarifData.map((tarif,index)=>
+     <TarifCards key = {index} {...tarif}/>
+      )}
+    </React.Fragment>
+);
 }
-export default CardStyleTrafic;
