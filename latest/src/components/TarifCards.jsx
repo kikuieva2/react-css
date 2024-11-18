8fd60ced1791136e78863aa175f1a727032c15be
kikuieva2 <140496966+kikuieva2@users.noreply.github.com>
2024-11-18 17:48:28 +0200

    Update TarifCards.jsx

--- latest/src/components/TarifCards.jsx
+++ latest/src/components/TarifCards.jsx
@@ -1,23 +1,24 @@
+
 import styles from "../styles/TarifCards.module.css";
 
-function TarifCards( {title, price, megabaits, description, colorClass, isHighlighted }) {
-    const cardClass = `${styles.tarifCards} ${styles[colorClass]} ${isHighlighted ? styles.highlighted : ''}`;
-    return(
-        <div className={cardClass}>
-            <header>
-                <h4 className={styles.tariftitle}>{title}</h4>
-            </header>
-            <main>
-                <h5 className={styles.tarifprice}>{price}</h5>
-            </main>
-            <section>
-                <h4 className={styles.tarifmegabaits}>{megabaits}</h4>
-            </section>
-            <footer>
-                <h4 className={styles.tarifdescription}>{description}</h4>
-            </footer>
-        </div>
-    );
+function TarifCards({title,price, megabaits,description,colorClass,isHighlighted = false}){
+    const cardClass = `${styles.tarifCards} ${styles[colorClass]} ${isHighlighted ? styles.highlighted : ''}`
+return(
+<div className={cardClass}>
+    <header>
+        <h4 className={styles.tarifTitle}>{title}</h4>
+    </header>
+    <main>
+        <h5 className={styles.tarifPrice}>{price}</h5>
+    </main>
+    <section>
+        <h4 className={styles.tarifMegaBaits}>{megabaits}</h4>
+    </section>
+<footer>
+    <h4 className={styles.tarifDescription}>{description}</h4>
+</footer>
+</div>
+)
 }
-
 export default TarifCards;
+
